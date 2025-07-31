a6fb8dd3962815dc1ffdfbf659aa16f1cdcf20ca
MhaskeAditya42 <adityamhaske711@gmail.com>
2025-07-31 11:20:03 +0000

    Connected the Database

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,22 @@
 
+const express = require('express');
+
+const {createPool} = require("mysql");
+
+const pool = createPool({
+    host:"localhost",
+    user : "root",
+    password : "n3u3da!",
+    database : "portfolio_manager",
+    connectionLimit : "10"
+});
+
+pool.query("Select * from stock_prices where price_id = ?",['8'],(err,result,fields)=>{
+    if(err){
+        return err;
+    }
+    console.log(result);
+})
+
+
 
