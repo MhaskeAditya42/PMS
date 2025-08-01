350fe7b3cee16eb8aed7732eeccc9cd66eb32205
Aditya Mhaske <adityamhaske@visisense.in>
2025-08-02 01:29:39 +0530

    completed stocks api

--- backend/controllers/stockController.js
+++ backend/controllers/stockController.js
@@ -17,5 +17,36 @@ exports.getAllStocks = (req, res) => {
     });
   };
 
+  exports.createStock = (req, res) => {
+  const { symbol, isin,series } = req.body;
+  db.query('INSERT INTO stocks (symbol, isin, series) VALUES (?, ?,?)', [symbol, isin,series], (err, result) => {
+    if (err) return res.status(500).json({ message: 'Error creating stock', error: err });
+    res.status(201).json({ id: result.insertId, symbol, isin });
+  });
+}
+
+exports.updateStock = (req, res) => {
+  const stockId = req.params.id;
+  const { symbol, isin, series} = req.body;
+
+  if (!symbol || !isin || !series) {
+    return res.status(400).json({ message: 'Missing required fields' });
+  }
+
+  db.query('UPDATE stocks SET symbol = ?, isin = ? , series= ? WHERE stock_id = ?', [symbol, isin, series, stockId], (err) => {
+    if (err) return res.status(500).json({ message: 'Error updating stock', error: err });
+    res.json({ message: 'Stock updated' });
+  });
+};
+
+exports.deleteStock = (req, res) => {
+  const stockId = req.params.id;
+  db.query('DELETE FROM stocks WHERE stock_id = ?', [stockId], (err) => {
+    if (err) return res.status(500).json({ message: 'Error deleting stock', error: err });
+    res.json({ message: 'Stock deleted' });
+  });
+};
+
+
   
 
--- backend/routes/stockRoutes.js
+++ backend/routes/stockRoutes.js
@@ -4,5 +4,8 @@ const stockController = require('../controllers/stockController');
 
 router.get('/',stockController.getAllStocks);
 router.get('/:id', stockController.getStockById);
+router.post('/', stockController.createStock);
+router.put('/:id', stockController.updateStock);
+router.delete('/:id', stockController.deleteStock);
 
 module.exports= router;
